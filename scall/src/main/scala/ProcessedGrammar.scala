d6e6a8e749b58fcb0e510f79ee475e87aab81ecb
PastaCodes <marco.j.buda@gmail.com>
2026-07-06 23:11:03 +0200

    feat(processed): process alternation alternatives

--- scall/src/main/scala/ProcessedGrammar.scala
+++ scall/src/main/scala/ProcessedGrammar.scala
@@ -11,3 +11,4 @@ type Alternatives = Set[SymbolSeq]
 object Alternatives:
   def ofTerminal(s: Terminal): Alternatives = Set(Seq(s))
   def ofNonterminal(s: Rule): Alternatives = Set(Seq(s))
+  def ofAlternation(t1: Alternatives, t2: Alternatives): Alternatives = t1.union(t2)
--- scall/src/test/scala/ProcessedGrammarTest.scala
+++ scall/src/test/scala/ProcessedGrammarTest.scala
@@ -14,14 +14,35 @@ class ProcessedGrammarTest extends AnyFunSuite:
 
   // noinspection TypeAnnotation, ForwardReference
   object SimpleGrammar extends Grammar:
-    val start = -> (alternation)
+    val start = -> (alternationSimple)
     val a = -> ("a")
     val b = -> ("b")
+    val c = -> ("c")
+    val d = -> ("d")
     val concat = a ++ b
-    val alternation = a | b
+    val alternationSimple = a | b
+    val alternationNested = (a | b) | (b | c)
     val optional = a.?
     val zeroOrMore = a.*
     val oneOrMore = a.+
 
   test("nonterminal is processed as a single alternative"):
     Alternatives.ofNonterminal(SimpleGrammar.start) shouldBe Set(Seq(SimpleGrammar.start))
+
+  test("alternation of terminals is processed as two alternatives"):
+    val aAlt = Alternatives.ofTerminal(SimpleGrammar.a)
+    val bAlt = Alternatives.ofTerminal(SimpleGrammar.b)
+    Alternatives.ofAlternation(aAlt, bAlt) shouldBe Set(
+      Seq(SimpleGrammar.a), Seq(SimpleGrammar.b)
+    )
+
+  test("alternation is processed as the union of alternatives"):
+    val aAlt = Alternatives.ofTerminal(SimpleGrammar.a)
+    val bAlt = Alternatives.ofTerminal(SimpleGrammar.b)
+    val cAlt = Alternatives.ofTerminal(SimpleGrammar.c)
+    val dAlt = Alternatives.ofTerminal(SimpleGrammar.d)
+    val alt1 = Alternatives.ofAlternation(aAlt, bAlt)
+    val alt2 = Alternatives.ofAlternation(cAlt, dAlt)
+    Alternatives.ofAlternation(alt1, alt2) shouldBe Set(
+      Seq(SimpleGrammar.a), Seq(SimpleGrammar.b), Seq(SimpleGrammar.c), Seq(SimpleGrammar.d)
+    )
