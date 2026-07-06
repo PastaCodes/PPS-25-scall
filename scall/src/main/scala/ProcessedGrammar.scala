0ae7b021c4cbf5ad79589f497c0e02efad0e342c
PastaCodes <marco.j.buda@gmail.com>
2026-07-06 22:52:02 +0200

    feat(processed): process nonterminal alternatives

--- scall/src/main/scala/ProcessedGrammar.scala
+++ scall/src/main/scala/ProcessedGrammar.scala
@@ -2,11 +2,12 @@ package it.unibo.scall
 
 import Element.*
 
-case class InternalNonTerminal()
-type AnyNonTerminal = Rule | InternalNonTerminal
-type AnySymbol = Terminal | AnyNonTerminal
+case class InternalNonterminal()
+type AnyNonterminal = Rule | InternalNonterminal
+type AnySymbol = Terminal | AnyNonterminal
 type SymbolSeq = Seq[AnySymbol]
 type Alternatives = Set[SymbolSeq]
 
 object Alternatives:
   def ofTerminal(s: Terminal): Alternatives = Set(Seq(s))
+  def ofNonterminal(s: Rule): Alternatives = Set(Seq(s))
--- scall/src/test/scala/ProcessedGrammarTest.scala
+++ scall/src/test/scala/ProcessedGrammarTest.scala
@@ -11,3 +11,17 @@ class ProcessedGrammarTest extends AnyFunSuite:
 
   test("terminal is processed as a single alternative"):
     Alternatives.ofTerminal(EmptyGrammar.start) shouldBe Set(Seq(EmptyGrammar.start))
+
+  // noinspection TypeAnnotation, ForwardReference
+  object SimpleGrammar extends Grammar:
+    val start = -> (alternation)
+    val a = -> ("a")
+    val b = -> ("b")
+    val concat = a ++ b
+    val alternation = a | b
+    val optional = a.?
+    val zeroOrMore = a.*
+    val oneOrMore = a.+
+
+  test("nonterminal is processed as a single alternative"):
+    Alternatives.ofNonterminal(SimpleGrammar.start) shouldBe Set(Seq(SimpleGrammar.start))
