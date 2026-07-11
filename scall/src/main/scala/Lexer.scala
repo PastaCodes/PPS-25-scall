c25b914defac1e74f7d94b360fd98fed1f576483
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2026-07-12 00:42:14 +0200

    feat(maxMatch): added lexer first TDD interaction

---
+++ scall/src/main/scala/Lexer.scala
@@ -0,0 +1,7 @@
+package it.unibo.scall
+
+import Element.*
+
+class Lexer(terminals: Seq[Terminal]):
+  def tokenize(input: String): List[Token] =
+    List.empty[Token]
---
+++ scall/src/test/scala/LexerTest.scala
@@ -0,0 +1,13 @@
+package it.unibo.scall
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.*
+import Element.*
+
+class LexerTest extends AnyFunSuite:
+  val terminals: Seq[Terminal] = Seq(TextTerminal("if"))
+
+  test("Lexer should return empty seq for empty string"):
+    val lexer = Lexer(Seq.empty)
+    lexer.tokenize("") shouldBe empty
+    
