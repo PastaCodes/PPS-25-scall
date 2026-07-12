830e53ac41f70cf00fce6151e789543fac967db7
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2026-07-12 11:29:46 +0200

    refactor(maxMatch): made tests clearer

--- scall/src/test/scala/LexerTest.scala
+++ scall/src/test/scala/LexerTest.scala
@@ -5,50 +5,38 @@ import org.scalatest.matchers.should.Matchers.*
 import Element.*
 
 class LexerTest extends AnyFunSuite:
-  val terminals: Seq[Terminal] = Seq(TextTerminal("if"))
+  val emptyLexer = Lexer(Seq.empty)
+  val ifRule: Terminal = TextTerminal("if")
+  val ifLexer = Lexer(Seq(ifRule))
+  val idRule: Terminal = RegexTerminal("[a-z]+".r)
+  val idLexer = Lexer(Seq(idRule))
 
   test("Lexer should return empty seq for empty string"):
-    val lexer = Lexer(Seq.empty)
-    lexer.tokenize("") shouldBe empty
+    emptyLexer.tokenize("") shouldBe empty
 
   test("Lexer recognize TextTerminal"):
-    val ifRule: Terminal = TextTerminal("if")
-    val input = "if"
-
-    val lexer = Lexer(Seq(ifRule))
-    val tokens = lexer.tokenize(input)
+    val tokens = ifLexer.tokenize("if")
     tokens.map(_.lexeme) shouldBe List("if")
 
   test("Lexer recognize RegexTerminal"):
-    val idPattern: Terminal = RegexTerminal("[a-z]+".r)
-    val input = "test"
-
-    val lexer = Lexer(Seq(idPattern))
-    val tokens = lexer.tokenize(input)
+    val tokens = idLexer.tokenize("test")
     tokens.map(_.lexeme) shouldBe List("test")
 
   test("Lexer implements longest-prefix-match"):
-    val ifRule: Terminal = TextTerminal("if")
-    val idPattern: Terminal = RegexTerminal("[a-z]+".r)
-    val input = "if"
-
-    val lexer = Lexer(Seq(ifRule, idPattern))
+    val terminals = Seq(ifRule, idRule)
+    val lexer = Lexer(terminals)
     val tokens = lexer.tokenize("iffy")
     tokens.map(_.lexeme) shouldBe List("iffy")
-    tokens.map(_.terminal) shouldBe List(idPattern)
+    tokens.map(_.terminal) shouldBe List(idRule)
 
   test("Lexer should use declaration order as the tie-breaker mechanism"):
-    val ifRule: Terminal = TextTerminal("if")
-    val idTerm: Terminal = RegexTerminal("[a-z]+".r)
-    val input = "if"
-
-    val lexer1 = Lexer(Seq(ifRule, idTerm))
-    val tokens1 = lexer1.tokenize(input)
+    val lexer1 = Lexer(Seq(ifRule, idRule))
+    val tokens1 = lexer1.tokenize("if")
     tokens1.map(_.terminal) shouldBe List(ifRule)
 
-    val lexer2 = Lexer(Seq(idTerm, ifRule))
-    val tokens2 = lexer2.tokenize(input)
-    tokens2.map(_.terminal) shouldBe List(idTerm)
+    val lexer2 = Lexer(Seq(idRule, ifRule))
+    val tokens2 = lexer2.tokenize("if")
+    tokens2.map(_.terminal) shouldBe List(idRule)
 
     val specificRegex: Terminal = RegexTerminal("ab".r)
     val generalRegex: Terminal = RegexTerminal("[a-z]+".r)
