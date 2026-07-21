76d9dfb0e3aee40fb4ad3e89a0da72fecc033adc
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2026-07-21 15:00:16 +0200

    feat(CSTNode): added nonTerminal and internalNonTerminal testing

--- scall/src/test/scala/ast/CSTNodeTest.scala
+++ scall/src/test/scala/ast/CSTNodeTest.scala
@@ -2,9 +2,9 @@ package it.unibo.scall.ast
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.*
-import it.unibo.scall.Element.Terminal
-import it.unibo.scall.Token
-import it.unibo.scall.ast.CSTNode.LeafNode
+import it.unibo.scall.Element.{Eps, Nonterminal, Terminal}
+import it.unibo.scall.{InternalNonterminal, Token}
+import it.unibo.scall.ast.CSTNode.{LeafNode, RuleNode}
 
 class CSTNodeTest extends AnyFunSuite:
 
@@ -14,3 +14,13 @@ class CSTNodeTest extends AnyFunSuite:
     val leaf: LeafNode = CSTNode.LeafNode(token)
 
     leaf.token.lexeme shouldBe "test"
+
+  test("CSTNode.RuleNode accepts both Nonterminal and InternalNonterminal"):
+    val standardRule: Nonterminal = Nonterminal("expr", () => Eps)
+    val internalRule = InternalNonterminal("expr_rep")
+
+    val node1: RuleNode = CSTNode.RuleNode(standardRule, Seq.empty)
+    val node2: RuleNode = CSTNode.RuleNode(internalRule, Seq.empty)
+
+    node1.symbol shouldBe standardRule
+    node2.symbol shouldBe internalRule
