40bb745ac5395db4a07e39652432e957bda99c44
Jacopo Turchi <jacopo.turchi@studio.unibo.it>
2026-08-23 17:32:45 +0200

    feat(finf): added ADT of AST for the finf grammar

---
+++ finf/ast/FinfNode.scala
@@ -0,0 +1,31 @@
+package it.unibo.finf
+package ast
+
+sealed trait FinfNode
+
+case class Program(declarations: Seq[Declaration], body: Expr) extends FinfNode
+
+sealed trait Declaration extends FinfNode
+case class RecordDecl(name: String, params: Seq[Parameter]) extends Declaration
+case class FunDecl(name: String, returnType: TypeRef, params: Seq[Parameter], localDecls: Seq[Declaration], body: Expr) extends Declaration
+case class ValDecl(name: String, tpe: TypeRef, value: Expr) extends Declaration
+
+case class Parameter(name: String, tpe: TypeRef) extends FinfNode
+
+sealed trait TypeRef extends FinfNode
+case object IntType extends TypeRef
+case object BoolType extends TypeRef
+case class CustomType(name: String) extends TypeRef
+
+sealed trait Expr extends FinfNode
+case class IntLit(value: Int) extends Expr
+case class BoolLit(value: Boolean) extends Expr
+case object NullLit extends Expr
+case class Id(name: String) extends Expr
+case class FieldAccess(record: String, field: String) extends Expr
+case class Call(func: String, args: Seq[Expr]) extends Expr
+case class New(record: String, args: Seq[Expr]) extends Expr
+case class Print(args: Seq[Expr]) extends Expr
+case class BinaryOp(op: String, left: Expr, right: Expr) extends Expr
+case class UnaryOp(op: String, expr: Expr) extends Expr
+case class If(cond: Expr, thenBranch: Expr, elseBranch: Expr) extends Expr
